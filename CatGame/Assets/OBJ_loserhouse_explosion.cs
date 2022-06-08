2ef724ce3c5a936eaceddbd211de9d5d03437246
trashgamedev <thefreshkeeper@gmail.com>
2022-06-08 21:24:17 +1000

    Animation and Documentation

---
+++ CatGame/Assets/OBJ_loserhouse_explosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OBJ_loserhouse_explosion : MonoBehaviour
+{
+	
+	private Animator animator;
+
+    void Awake()
+    {
+		animator = GetComponent<Animator>();
+		animator.SetTrigger("Destroyed");
+		StartCoroutine(Die());
+    }
+	
+	private IEnumerator Die()
+	{
+		yield return new WaitForSecondsRealtime(0.65f);
+		Destroy(this.gameObject);
+	}
+
+   
+}
